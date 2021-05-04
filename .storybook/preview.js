fc51488fb537a77932ee4962960d0ebb0efbe3ee
Marek Polak <polak.marek@gmail.com>
2021-05-04 10:20:07 +0200

    configure storybook-i18next-addon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,16 @@
+import React, { Suspense } from 'react';
+import { I18nextProvider, initReactI18next } from 'react-i18next';
+import { withI18next } from 'storybook-addon-i18next';
+
+import i18n from 'i18next';
+import Backend from 'i18next-http-backend';
+
+i18n
+  .use(Backend)
+  .use(initReactI18next)
+  .init({
+  fallbackLng: 'en'
+});
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -8,3 +21,8 @@ export const parameters = {
     },
   },
 }
+
+export const decorators = [
+  withI18next({ i18n, languages: { en: 'English', sk: 'Slovak' }}),
+  Story => <Suspense fallback='loading...'><Story /></Suspense>
+];
